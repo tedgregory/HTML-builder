73bce92b895b81614b2ac59ed08ce407f5710b60
Ted McGregor <tegmcgregor@zohomail.com>
2022-05-23 00:59:45 +0300

    feat: 04 works recursively

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -10,10 +10,11 @@ const copyFile = util.promisify(fs.copyFile);
 const originalDir = path.resolve(__dirname, 'files');
 const destinationDir = path.resolve(__dirname, 'files-copy');
 
-copyFiles(originalDir, destinationDir, true);
+copyFiles(originalDir, destinationDir);
 
-async function copyFiles(fromDir, toDir, reset = false) {
-  reset && await resetDestinationDir(toDir);
+// works recursively
+async function copyFiles(fromDir, toDir) {
+  await resetDestinationDir(toDir);
   const items = await readdir(fromDir, {withFileTypes: true});
   for (let item of items) {
     let 
